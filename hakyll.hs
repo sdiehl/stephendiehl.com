385d0679d18bdde3756fc4a2cc3a055a7f497cfb
Stephen Diehl <stephen.m.diehl@gmail.com>
2014-01-31 15:44:47 -0600

    remove dead code

--- hakyll.hs
+++ hakyll.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 --------------------------------------------------------------------
 -- |
 -- Copyright :  (c) Stephen Diehl 2014
@@ -114,55 +115,3 @@ main = hakyllWith cfg $ do
   archive
   index
   templates
-
-{-main :: IO ()-}
-{-main = hakyll $ do-}
-    {-match "images/*" $ do-}
-        {-route   idRoute-}
-        {-compile copyFileCompiler-}
-
-    {-match "css/*" $ do-}
-        {-route   idRoute-}
-        {-compile compressCssCompiler-}
-
-    {-match "files/*" $ do-}
-        {-route   idRoute-}
-        {-compile copyFileCompiler-}
-
-    {-match "templates/*" $ compile templateCompiler-}
-
-    {-match "index.md" $ do-}
-        {-route   $ setExtension "html"-}
-        {-compile $ pageCompiler-}
-            {->>> applyTemplateCompiler "templates/default.html"-}
-            {->>> relativizeUrlsCompiler-}
-
-    {-match "posts/*" $ do-}
-        {-route   $ setExtension "html"-}
-        {-compile $ py_pre-}
-            {->>> arr readPage-}
-            {->>> addDefaultFields-}
-            {->>> pageRenderPandocWith defaultHakyllParserState pandocOptions-}
-            {->>> applyTemplateCompiler "templates/default.html"-}
-            {->>> relativizeUrlsCompiler-}
-
-    {-match "pages/*" $ do-}
-        {-route   $ setExtension "html"-}
-        {-compile $ pageCompiler-}
-            {->>> applyTemplateCompiler "templates/default.html"-}
-            {->>> relativizeUrlsCompiler-}
-
-    {-match "posts.html" $ route idRoute-}
-    {-create "posts.html" $ constA mempty-}
-        {->>> arr (setField "title" "All posts")-}
-        {->>> requireAllA "posts/*" addPostList-}
-        {->>> applyTemplateCompiler "templates/list.html"-}
-        {->>> applyTemplateCompiler "templates/default.html"-}
-        {->>> relativizeUrlsCompiler-}
-
-{-addPostList :: Compiler (Page String, [Page String]) (Page String)-}
-{-addPostList = setFieldA "posts" $-}
-    {-arr chronological-}
-        {->>> require "templates/post.html" (\p t -> map (applyTemplate t) p)-}
-        {->>> arr mconcat-}
-        {->>> arr pageBody-}
